5833de218a7491439fd0c863289ff742d1a7b2e4
FelixRandle <felix@randle.org>
2018-09-20 08:16:43 +0100

    Larger Update

--- application.py
+++ application.py
@@ -25,7 +25,8 @@ class Application(tk.Tk):
                 # Get the current directory.
                 cd = os.getcwd()
                 # Set our programs icon for the top left.
-                self.iconbitmap(cd+"/assets/maze.ico")
+                # Disabled for Linux development. Re-enable for deployment.
+                #self.iconbitmap(cd+"/assets/maze.ico")
                         
                 # Stop the user from resizing the screen.
                 self.screenSize = 750
@@ -37,7 +38,7 @@ class Application(tk.Tk):
                 self.loadStyles()
 
                 # Ensure all placed items are centered.
-                self.grid_columnconfigure(0, weight=1)
+                self.grid_columnconfigure(0, weight = 1)
 
                 # Create a dictionary for the screen tabs and populate it.
                 self.frames = {}
@@ -45,34 +46,63 @@ class Application(tk.Tk):
                 # Load in all tabs and put them in our dictionary.
                 self.frames[HomeScreen] = HomeScreen(self)
                 self.frames[MazeScreen] = MazeScreen(self)
-                self.frames[GenerationSettings] = GenerationSettings(self)
 
                 # Load the homescreen
                 self.changeFrame(HomeScreen)
+
+                self.loadTopMenu()
 
                 # Create a dictionary for the side menus and populate it.
                 self.menus = {}
 
                 # Load in all menus and put them in our dictionary.
                 #self.menus[GeneratorMenu] = GeneratorMenu(self)
-                self.menus[SolverMenu] = SolverMenu(self)
+                self.menus[SolverSettings] = SolverSettings(self)
+                self.menus[GenerationSettings] = GenerationSettings(self)
 
                 # Load Maze 
                 self.loadMaze()
 
         def loadStyles(self):
+                # Load a style object
                 self.style = ttk.Style()
+
+                # Load in styles for any situation we need.
+
                 self.style.configure("Title.TLabel", font = ("Helvetica", 30, "bold italic"))
 
                 self.style.configure("Footer.TLabel", font = ("Times", 12, "roman"))
 
+        def loadTopMenu(self):
+                """
+                Method for loading in the top menu of the screen.
+                """
+                menubar = tk.Menu(self)
+                self.config(menu = menubar)
+
+                fileMenu = tk.Menu(menubar, tearoff = False)
+                fileMenu.add_command(label = "Save Maze", command = lambda: print("Save Maze"))
+                fileMenu.add_command(label = "Load Maze", command = lambda: print("Load Maze"))
+                fileMenu.add_separator()
+                fileMenu.add_command(label = "Save Solve", command = lambda: print("Save Solve"))
+                fileMenu.add_command(label = "Load Solve", command = lambda: print("Load Solve"))
+                menubar.add_cascade(label = "File", menu = fileMenu)
+
+                menubar.add_command(label = "Generate Maze", command = lambda: self.changeMenu(GenerationSettings))
+
+                menubar.add_command(label = "Solve Current Maze", command = lambda: self.changeMenu(SolverSettings))
+
+                editMenu = tk.Menu(menubar, tearoff = False)
+                self.editMode = tk.BooleanVar()
+                editMenu.add_checkbutton(label = "Toggle edit mode", onvalue = True, offvalue = False, variable = self.editMode)
+                menubar.add_cascade(label = "Edit", menu = editMenu)
 
 
         def changeFrame(self, newFrame):
                 """
                 Internal method for changing the current frame shown on screen.
                 Arguments:
-                newFrame -- The frame to change to
+                        newFrame -- The frame to change to
                 """
                 #Loop through all frames that are currently on the grid and remove them from the grid
                 for frame in self.grid_slaves():
@@ -87,30 +117,42 @@ class Application(tk.Tk):
                 """
                 Internal method for changing the current frame shown on screen.
                 Arguments:
-                newFrame -- The frame to change to
+                        newFrame -- The frame to change to
                 """
                 #Loop through all frames that are currently on the grid and remove them from the grid
                 for frame in self.grid_slaves():
-                        if frame.grid_info()["column"] == 0:
+                        if frame.grid_info()["column"] == 1:
                                 frame.grid_forget()
-                #Load the new frame
-                frame = self.frames[newFrame]
-                #Place our new frame onto the grid
-                frame.grid(row = 0, column = 0)
+
+                if newMenu != None:
+                        #Load the new frame
+                        frame = self.menus[newMenu]
+                        #Place our new frame onto the grid
+                        frame.grid(row = 0, column = 1, sticky = "N")
 
         def loadMaze(self, size = 51):
                 """
                 Load in a blank Maze object.
                 Arguments:
-                size -- The width and height of the new Maze
+                        size -- The width and height of the new Maze
                 """
+                # Load the frame we are going to attach it to.
                 frame = self.frames[MazeScreen]
                 self.maze = maze.Maze(frame, canvasSize = self.screenSize, size = size)
                 self.maze.canvas.grid(row = 0, column = 0)
 
+        def resetMaze(self, size):
+                """
+                Destroy old maze and load a new maze with the given size
+                Arguments:
+                        size -- The width and height of the new maze.
+                """
+                self.maze.canvas.destroy()
+                self.loadMaze(size)
+
         def generateMaze(self):
                 # Copy the GenerationSettings for easier referencing.
-                settings = self.frames[GenerationSettings]
+                settings = self.menus[GenerationSettings]
 
                 # Get the algorithm choice and load that algorithms Generator.
                 algorithm = settings.algorithmChoice.get()
@@ -124,22 +166,15 @@ class Application(tk.Tk):
                 # Load in the size of the maze from settings
                 size = int(settings.mazeSize.get())
 
-                # Ask the user for conformation to ensure they don't overwrite a maze they're working on.
-                if not (mb.askyesno(self.title, "Are you sure you want to generate a new maze?")):
-                        return
-
                 # Reset the current maze and generate a new one from the loaded generator.
                 self.resetMaze(size)
 
+                # Use the generator to create a new maze.
                 Generator(self.maze)
-
-        def resetMaze(self, size):
-                self.maze.canvas.destroy()
-                self.loadMaze(size)
 
         def solveMaze(self):
                 # Copy the GenerationSettings for easier referencing.
-                settings = self.parent.frames[SolverSettings]
+                settings = self.frames[SolverSettings]
 
                 # Get the algorithm choice and load that algorithms Generator.
                 algorithm = settings.solverChoice.get()
@@ -150,6 +185,7 @@ class Application(tk.Tk):
                         print("No item selected")
                         return
 
+                # Use the solver to solve our maze.
                 Solver(self.maze)
 
 class HomeScreen(tk.Frame):
@@ -164,8 +200,13 @@ class HomeScreen(tk.Frame):
                 self.loadGUI()
 
         def loadGUI(self):
-                self.title = ttk.Label(self, text = "Path Finding Thing", style = "Title.TLabel")
-                self.title.grid(row = 0, column = 0, pady = 75)
+                self.titleImage = tk.PhotoImage(file = "assets/title.png")
+                self.title = ttk.Label(self, image = self.titleImage, text = "Path Finding Thing", style = "Title.TLabel")
+                self.title.grid(row = 0, column = 0, pady = 50)
+
+                self.settingsImage = tk.PhotoImage(file = "assets/settings.png")
+                self.settingsButton = tk.Button(self, image = self.settingsImage, command = lambda: self.parent.changeMenu(GenerationSettings), borderwidth = 0)
+                self.settingsButton.grid(row = 0, column = 0, sticky = "NE")
 
                 self.generateImage = tk.PhotoImage(file = "assets/homeGenerate.png")
                 self.generateButton = tk.Button(self, image = self.generateImage, command = self.generateMaze, borderwidth = 0)
@@ -176,7 +217,7 @@ class HomeScreen(tk.Frame):
                 self.loadButton.grid(row = 2, column = 0, pady = 30)
 
                 self.footer = ttk.Label(self, text = "Created By Felix J. Randle", style = "Footer.TLabel")
-                self.footer.grid(row = 10, column = 0, sticky = "S", pady = 100)
+                self.footer.grid(row = 10, column = 0, sticky = "S", pady = 50)
 
         def generateMaze(self):
                 print("Generating Maze")
@@ -194,62 +235,29 @@ class MazeScreen(tk.Frame):
                 """
                 self.parent = parent
 
-                self.loadTopMenu()
-
-        def loadTopMenu(self):
-                """
-                Method for loading in the top menu of the screen.
-                """
-                menubar = tk.Menu(self)
-                self.parent.config(menu = menubar)
-
-                fileMenu = tk.Menu(menubar, tearoff = False)
-                fileMenu.add_command(label = "Save Maze", command = lambda: print("Save Maze"))
-                fileMenu.add_command(label = "Load Maze", command = lambda: print("Load Maze"))
-                fileMenu.add_separator()
-                fileMenu.add_command(label = "Save Solve", command = lambda: print("Save Solve"))
-                fileMenu.add_command(label = "Load Solve", command = lambda: print("Load Solve"))
-                menubar.add_cascade(label = "File", menu = fileMenu)
-
-                #generatorMenu = tk.Menu(menubar, tearoff = False)
-                menubar.add_command(label = "Generate Maze", command = self.parent.generateMaze)
-                #menubar.add_cascade(label = "Generate", menu = generatorMenu)
-
-                solverMenu = tk.Menu(menubar, tearoff = False)
-                solverMenu.add_command(label = "Solve Current Maze", command = self.parent.solveMaze)
-                solverMenu.add_command(label = "Edit Solve Settings", command = lambda: self.parent.changeFrame(SolverSettings))
-                menubar.add_cascade(label = "Solve", menu = solverMenu)
-
-                editMenu = tk.Menu(menubar, tearoff = False)
-                self.editMode = tk.BooleanVar()
-                editMenu.add_checkbutton(label = "Toggle edit mode", onvalue = True, offvalue = False, variable = self.editMode)
-                menubar.add_cascade(label = "Edit", menu = editMenu)
-
-"""
 class SettingsMenu(tk.Frame):
         def __init__(self, parent):
                 super().__init__()
                 self.parent = parent
 
-                self.buttonImage = tk.PhotoImage(file = "assets/home_button.png")
+                self.exitImage = tk.PhotoImage(file = "assets/exit.png")
+                self.exitButton = tk.Button(self, image = self.exitImage, command = lambda: parent.changeMenu(None), borderwidth = 0)
+                self.exitButton.grid(row = 0, column = 0, sticky = "NE")
 
-                xScale = Fraction(parent.screenSize / self.buttonImage.width()).limit_denominator(max_denominator=100)
-                yScale = Fraction((parent.screenSize / self.buttonImage.height()) * 0.1).limit_denominator(max_denominator=100)
+        def loadTitle(self, source):
+                """
+                Load a title image from the given source.
+                """
+                self.titleImage = tk.PhotoImage(file = source)
 
-                self.buttonImage = self.buttonImage.zoom(xScale.numerator, yScale.numerator)
-                self.buttonImage = self.buttonImage.subsample(xScale.denominator, yScale.denominator)
+                tk.Label(self, image = self.titleImage).grid(row = 0, column = 0, pady = 15)
 
-                self.homeButton = tk.Button(self, image = self.buttonImage, command = lambda: parent.changeFrame(HomeScreen))
-                self.homeButton.grid(row = 0, column = 0)
-
-                self.TitleStyle = ttk.Style()
-                self.TitleStyle.configure("Title.TLabel", font = ("Helvetica", 30))
-"""      
-
-class GenerationSettings(tk.Frame):
+class GenerationSettings(SettingsMenu):
         def __init__(self, parent):
-                super().__init__()
+                super().__init__(parent)
                 self.parent = parent
+
+                self.loadTitle("assets/generationTitle.png")
 
                 ttk.Label(self, text = "Generation Algorithm", style = "Title.TLabel").grid(row = 1, column = 0, pady = 20)
 
@@ -292,9 +300,9 @@ class GenerationSettings(tk.Frame):
                 self.parent.changeFrame(HomeScreen)
                 self.parent.frames[HomeScreen].generateMaze()
 
-class SolverMenu(tk.Frame):
+class SolverSettings(SettingsMenu):
         def __init__(self, parent):
-                super().__init__()
+                super().__init__(parent)
 
                 ttk.Label(self, text = "Solver Algorithm", style = "Title.TLabel").grid(row = 1, column = 0, pady = 20)
 
